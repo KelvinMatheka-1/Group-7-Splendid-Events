7b8449abcbab44d0e50292b2df47776c00a9db61
Kelvin M Matheka <beingmatheka@gmail.com>
2022-12-30 17:23:59 +0300

    fixing data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_140930) do
+ActiveRecord::Schema.define(version: 2022_12_30_141821) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 2022_12_30_140930) do
     t.string "event_name"
     t.string "event_image"
     t.string "description"
-    t.integer "time"
+    t.time "time"
     t.string "location"
-    t.integer "date"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_12_30_140930) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type"
   end
 
 end
